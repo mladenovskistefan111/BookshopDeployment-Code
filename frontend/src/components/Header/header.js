d65fb0eabddc6b187f03b063c94749d6bf3910f5
mladenovskistefan111 <mladenovskistefan111@gmail.com>
2024-09-19 14:20:20 +0200

    Testing

--- frontend/src/components/Header/header.js
+++ frontend/src/components/Header/header.js
@@ -4,7 +4,7 @@ import {Navbar,Nav} from "react-bootstrap"
 const header = (props) => {
     return (
         <Navbar bg="dark" variant="dark" expand="lg" style={{ backgroundColor: '#333', fontFamily: 'Arial, sans-serif' }}>
-            <Navbar.Brand style={{ color: '#FFFFFF',fontFamily:"Roboto",fontSize:"20px"}} as={Link} to="/">BOOKSHOP LIBRARY 🖥️</Navbar.Brand>
+            <Navbar.Brand style={{ color: '#FFFFFF',fontFamily:"Roboto",fontSize:"20px"}} as={Link} to="/">BOOKSHOP 🖥️</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
